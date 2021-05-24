e985029fcafc8a3e85540f8a2869d2f3ab803023
chungguo <chungguo.gc@gmail.com>
2021-05-24 21:43:52 +0800

    feat: style

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Avatar from './Avatar'
 
 export default function Header() {
   return (
-    <header className="sticky w-full h-16 top-0 z-40 flex bg-white border-b">
+    <header className="w-full h-16 top-0 z-40 flex bg-white border-b">
       <Avatar />
       <Social styleNames="justify-end flex-1 hidden md:flex" />
     </header>

a9e1f5807b55f135111c73748e41def9a53dd869
chungguo <chungguo.gc@gmail.com>
2021-05-25 23:46:09 +0800

    feat: 替换信息

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,17 @@ export default function Index() {
         <meta property="og:url" content='https://chungguo.me/' />
         <meta property="og:description" content="I'm chungguo, coder, amateur photographer" />
       </Head>
-      <main className="w-full h-screen flex items-center justify-center flex-col">
-        <Link href="/blog">
-          <article className="cursor-pointer flex flex-col items-center justify-center text-center flex-1 -mt-32">
+      <main className="h-screen flex justify-center flex-col">
+        <article className="cursor-pointer flex flex-col items-center justify-center text-center flex-1 -mt-32">
+          <Link href="/blog">
             <img className="w-28 lg:w-40 md:32 inline rounded-full bg-gray-200" src="./chungguo.jpg" alt="avatar" />
-            <h2 className="text-base font-semibold py-4 lg:text-4xl md:text-2xl">
-              <p>👋 Hi, I'm chungguo</p>
-              <p><span className="text-indigo-600">coder, </span>amateur photographer.</p>
-            </h2>
-          </article>
-        </Link>
+          </Link>
+          <h2 className="text-base font-semibold py-4 lg:text-4xl md:text-2xl">
+            <p>👋 Hi, I'm chungguo</p>
+            <p><span className="text-indigo-600">coder, </span>amateur photographer.</p>
+          </h2>
+        </article>
+
         <Social styleNames="h-20 justify-center" />
       </main>
     </>

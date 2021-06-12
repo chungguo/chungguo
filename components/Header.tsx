a47a63c2ae00b9ea9c2d0f46260edecb9f0ff398
chungguo <chungguo.gc@gmail.com>
2021-06-12 20:48:14 +0800

    feat: home name

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="w-full h-16 top-0 z-40 flex bg-white border-b">
       <Avatar />
-      <Social styleNames="justify-end flex-1 hidden md:flex" />
+      <Social styleNames="justify-end flex-1 hidden md:flex" exclude={['blog']}/>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,15 @@ export default function Index() {
   return (
     <>
       <main className="h-screen flex justify-center flex-col">
-        <article className="cursor-pointer flex flex-col items-center justify-center text-center flex-1 -mt-32">
+        <article className="flex flex-col items-center justify-center text-center flex-1 -mt-32">
           <Link href="/blog">
-            <img className="w-28 lg:w-40 md:32 inline rounded-full bg-gray-200" src="./chungguo.jpg" alt="avatar" />
+            <img className="cursor-pointer w-28 lg:w-40 md:32 inline rounded-full bg-gray-200" src="./chungguo.jpg" alt="avatar" />
           </Link>
-          <h2 className="text-base font-semibold py-4 lg:text-4xl md:text-2xl">
+          <h2 className="text-3xl sm:text-4xl  lg:text-6xl  leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">
             <p>🖖 Hi, I'm chungguo</p>
-            <p><span className="text-indigo-600">coder, </span>amateur photographer.</p>
+            <p><span className="text-indigo-600">Coder, </span>Amateur Photographer.</p>
           </h2>
         </article>
-
         <Social styleNames="h-20 justify-center" />
       </main>
     </>
